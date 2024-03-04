e93bf31b3d3fd2bd519b111cf4d6b12dc8a4d0ca
vbhattac <53374818+vbhattaccmu@users.noreply.github.com>
2024-03-04 10:55:31 +0530

    feat: add pulsar

--- src/consumer.rs
+++ src/consumer.rs
@@ -1,151 +1,108 @@
 use log::info;
 use rand::Rng;
-use rdkafka::{
-    client::ClientContext,
-    config::ClientConfig,
-    consumer::{stream_consumer::StreamConsumer, CommitMode, Consumer, ConsumerContext, Rebalance},
-    error::KafkaResult,
-    message::{Headers, Message},
-    topic_partition_list::TopicPartitionList,
-};
 use std::{
     collections::HashMap,
     sync::Arc,
     time::{Duration, Instant},
 };
+use std::env;
 
-use crate::{config::Config, error::Error};
+use futures::TryStreamExt;
+use pulsar::{
+    authentication::oauth2::OAuth2Authentication, Authentication, Consumer, DeserializeMessage,
+    Payload, Pulsar, SubType, TokioExecutor,
+};
 
-struct CustomContext;
 
-impl ClientContext for CustomContext {}
+use crate::{config::Config, error::Error, producer::Task};
 
-impl ConsumerContext for CustomContext {
-    fn pre_rebalance(&self, rebalance: &Rebalance) {
-        info!("Pre rebalance {:?}", rebalance);
-    }
 
-    fn post_rebalance(&self, rebalance: &Rebalance) {
-        info!("Post rebalance {:?}", rebalance);
-    }
+impl DeserializeMessage for Task {
+    type Output = Result<Task, serde_json::Error>;
 
-    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
-        info!("Committing offsets: {:?}", result);
+    fn deserialize_message(payload: &Payload) -> Self::Output {
+        serde_json::from_slice(&payload.data)
     }
 }
 
 pub(crate) struct Workers {
     timeout: u64,
     group_ids: Vec<usize>,
-    consumer_groups: HashMap<usize, Vec<LoggingConsumer>>,
+    consumer: Consumer<Task, _>,
 }
 
 impl Workers {
     /// `init` creates a Worker Pool.
     pub(crate) fn init(config: Arc<Config>) -> Self {
-        let mut consumer_groups: HashMap<usize, Vec<LoggingConsumer>> = HashMap::new();
+        let addr = env::var("PULSAR_ADDRESS")
+        .ok()
+        .unwrap_or_else(|| "pulsar://127.0.0.1:6650".to_string());
+        let topic = env::var("PULSAR_TOPIC")
+            .ok()
+            .unwrap_or_else(|| "non-persistent://public/default/test".to_string());
 
-        // Custom logic:-
-        // Assign random group IDs to consumers such that
-        // different consumers belong to same consumer groups.
-        let group_ids: Vec<usize> = (0..config.num_workers)
-            .map(|_| rand::thread_rng().gen_range(1..=config.num_groups))
-            .collect();
+        let mut builder = Pulsar::builder(addr, TokioExecutor);
 
-        for worker in 0..config.num_workers {
-            let context = CustomContext;
-            let consumer: LoggingConsumer = ClientConfig::new()
-                .set("group.id", group_ids[worker].to_string())
-                .set("bootstrap.servers", config.brokers.join(","))
-                .set("enable.partition.eof", "false")
-                .set("session.timeout.ms", &config.consumer_timeout.to_string())
-                .set("enable.auto.commit", &config.enable_auto_commit.to_string())
-                .set("auto.offset.reset", "earliest")
-                .create_with_context(context)
-                .expect("Consumer creation failed");
+        if let Ok(token) = env::var("PULSAR_TOKEN") {
+            let authentication = Authentication {
+                name: "token".to_string(),
+                data: token.into_bytes(),
+            };
 
-            consumer_groups
-                .entry(group_ids[worker])
-                .or_insert(Vec::new())
-                .push(consumer);
+            builder = builder.with_auth(authentication);
+        } else if let Ok(oauth2_cfg) = env::var("PULSAR_OAUTH2") {
+            builder = builder.with_auth_provider(OAuth2Authentication::client_credentials(
+                serde_json::from_str(oauth2_cfg.as_str())
+                    .unwrap_or_else(|_| panic!("invalid oauth2 config [{}]", oauth2_cfg.as_str())),
+            ));
         }
+
+        let pulsar: Pulsar<_> = builder.build().await?;
+
+        let mut consumer: Consumer<Task, _> = pulsar
+            .consumer()
+            .with_topic(topic)
+            .with_consumer_name("test_consumer")
+            .with_subscription_type(SubType::Exclusive)
+            .with_subscription("test_subscription")
+            .build()
+            .await?;
 
         Workers {
             group_ids,
-            consumer_groups,
+            consumer,
             timeout: config.consumer_timeout,
         }
     }
 
-    /// `consume` denotes the core algorithm for load balancing on worker pool.
-    /// Strategy: 1. To balance CPU server load, each consumer in a group
-    ///              is allowed to subscribe to a topic till a timer elapses and offset
-    ///              is committed.
-    ///           2. Once timer has elapsed, next consumer in the group picks up where the
-    ///              previous consumer left off.
-    ///           3. Once all consumers are processed, we proceed to the next topic.
     pub(crate) async fn consume(&self, topics: Vec<String>) -> Result<(), Error> {
-        for (idx, topic) in topics.iter().enumerate() {
-            info!("Processing Topic: {}", topic);
-
-            let group_id = self.group_ids[idx];
-            if let Some(consumers) = self.consumer_groups.get(&group_id) {
-                for consumer in consumers.iter() {
-                    consumer
-                        .subscribe(&[topic])
-                        .map_err(|_| Error::ResourceBusy)?;
-
-                    // process messages till a timer elapses.
-                    self.process(consumer).await;
+        let mut counter = 0usize;
+        while let Some(msg) = consumer.try_next().await? {
+            consumer.ack(&msg).await?;
+            log::info!("metadata: {:?}", msg.metadata());
+            log::info!("id: {:?}", msg.message_id());
+            let data = match msg.deserialize() {
+                Ok(data) => data,
+                Err(e) => {
+                    log::error!("could not deserialize message: {:?}", e);
+                    break;
                 }
+            };
+    
+            if data.data.as_str() != "data" {
+                log::error!("Unexpected payload: {}", &data.data);
+                break;
+            }
+            counter += 1;
+            log::info!("got {} messages", counter);
+    
+            if counter > 10 {
+                consumer.close().await.expect("Unable to close consumer");
+                break;
             }
         }
 
         Ok(())
-    }
-
-    /// `process` allows the worker pool to process a
-    /// message received from a topic.
-    async fn process(&self, consumer: &StreamConsumer<CustomContext>) {
-        let start = Instant::now();
-        loop {
-            match consumer.recv().await {
-                Err(e) => {
-                    info!("Kafka error: {}", e);
-                    break;
-                }
-                Ok(msg) => {
-                    let payload = match msg.payload_view::<str>() {
-                        None => "",
-                        Some(Ok(s)) => s,
-                        Some(Err(e)) => {
-                            info!("Error while deserializing message payload: {:?}", e);
-                            break;
-                        }
-                    };
-
-                    info!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
-                        msg.key(), payload, msg.topic(), msg.partition(), msg.offset(), msg.timestamp());
-
-                    if let Some(headers) = msg.headers() {
-                        for header in headers.iter() {
-                            info!("Header {:#?}: {:?}", header.key, header.value);
-                        }
-                    }
-
-                    if let Err(e) = consumer.commit_message(&msg, CommitMode::Async) {
-                        info!("Commit error {:?}", e);
-                    };
-                }
-            };
-
-            if start.elapsed() > Duration::from_millis(self.timeout) {
-                info!("Timer elapsed");
-                // Let this consumer cool down a bit
-                // Let other members in the same group process the topic.
-                break;
-            }
-        }
     }
 }
 
--- src/producer.rs
+++ src/producer.rs
@@ -1,11 +1,11 @@
 use log::info;
-use rdkafka::{
-    message::{Header, OwnedHeaders},
-    producer::{FutureProducer, FutureRecord},
-    ClientConfig,
+use pulsar::{
+    authentication::oauth2::OAuth2Authentication, message::proto, producer, Authentication,
+    Error as PulsarError, Pulsar, SerializeMessage, TokioExecutor,
 };
 use serde::{Deserialize, Serialize};
 use std::{
+    env,
     sync::{Arc, Mutex},
     time::Duration,
 };
@@ -157,44 +157,91 @@ impl Producer {
     /// `produce_messages` produces messages to a
     /// predefined set of topics.
     async fn produce_messages(&self, config: Arc<Config>) -> Result<(), Error> {
-        let producer: &FutureProducer = &ClientConfig::new()
-            .set("bootstrap.servers", config.brokers.join(","))
-            .set("message.timeout.ms", config.producer_timeout.to_string())
-            .create()
-            .map_err(|_| Error::ProducerTimeout)?;
+        let addr = env::var("PULSAR_ADDRESS")
+            .ok()
+            .unwrap_or_else(|| "pulsar://127.0.0.1:6650".to_string());
+        let topic = env::var("PULSAR_TOPIC")
+            .ok()
+            .unwrap_or_else(|| "non-persistent://public/default/test".to_string());
+
+        let mut builder = Pulsar::builder(addr, TokioExecutor);
+
+        if let Ok(token) = env::var("PULSAR_TOKEN") {
+            let authentication = Authentication {
+                name: "token".to_string(),
+                data: token.into_bytes(),
+            };
+
+            builder = builder.with_auth(authentication);
+        } else if let Ok(oauth2_cfg) = env::var("PULSAR_OAUTH2") {
+            builder = builder.with_auth_provider(OAuth2Authentication::client_credentials(
+                serde_json::from_str(oauth2_cfg.as_str())
+                    .unwrap_or_else(|_| panic!("invalid oauth2 config [{}]", oauth2_cfg.as_str())),
+            ));
+        }
+
+        let pulsar: Pulsar<_> = builder.build().await?;
+        let mut producer = pulsar
+            .producer()
+            .with_topic(topic)
+            .with_name("my producer")
+            .with_options(producer::ProducerOptions {
+                schema: Some(proto::Schema {
+                    r#type: proto::schema::Type::String as i32,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+            .build()
+            .await?;
+
+        let mut counter = 0usize;
         let query_timeout = config.clone().query_timeout;
 
         // keep producing messages to topic
         loop {
-            let tasks = self.task_pool.lock().unwrap();
-            let futures = tasks
-                .iter()
-                .enumerate()
-                .map(|task| async move {
-                    let delivery_status = producer
-                        .send(
-                            FutureRecord::to(&task.1.topic)
-                                .payload(&format!("Message {}", task.1.message))
-                                .key(&format!("Key {}", task.0))
-                                .headers(OwnedHeaders::new().insert(Header {
-                                    key: "header_key",
-                                    value: Some("header_value"),
-                                })),
-                            Duration::from_secs(query_timeout),
-                        )
-                        .await;
-
-                    delivery_status
+            producer
+                .send(Task {
+                    topic,
+                    message: "a".to_string(),
                 })
-                .collect::<Vec<_>>();
+                .await?
+                .await
+                .unwrap();
 
-            for future in futures {
-                info!("Future completed. Result: {:?}", future.await);
+            counter += 1;
+            log::info!("{counter} messages");
+            tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+
+            if counter > 10 {
+                producer.close().await.expect("Unable to close connection");
+                break;
             }
-
-            drop(tasks);
-
-            tokio::time::sleep(Duration::from_secs(query_timeout)).await;
         }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct TestData {
+    data: String,
+}
+
+impl SerializeMessage for Task {
+    fn serialize_message(input: Self) -> Result<producer::Message, PulsarError> {
+        let payload = serde_json::to_vec(&input).map_err(|e| PulsarError::Custom(e.to_string()))?;
+        Ok(producer::Message {
+            payload,
+            ..Default::default()
+        })
+    }
+}
+
+impl SerializeMessage for TestData {
+    fn serialize_message(input: Self) -> Result<producer::Message, PulsarError> {
+        let payload = serde_json::to_vec(&input).map_err(|e| PulsarError::Custom(e.to_string()))?;
+        Ok(producer::Message {
+            payload,
+            ..Default::default()
+        })
     }
 }
